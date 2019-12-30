b01d398147425e9d3c862f25bf7b8eb0ef6e4e8d
takana-cake <msdf303@live.jp>
2019-12-30 21:52:59 +0900

    Update flaskserver.py

--- flaskserver.py
+++ flaskserver.py
@@ -1,38 +1,40 @@
-#v.20191230.0
+#v.20191230.1
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, Markup
+import sys, os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	html = '''
-	<form action="/test">
-		<p><label>test: </label>
-		<input type="text" name="url" value="url" size="30">
-		<input type="text" name="path" value="path" size="30">
-		<button type="submit" formmethod="get">GET</button>
+	<form action="/getimgs">
+		<p><label>getImgs: </label>
+		<input type="text" name="url" value="url" size="30" onclick="this.select(0,this.value.length)">
+		<input type="text" name="path" value="Folder name" size="30" onclick="this.select(0,this.value.length)">
 		<button type="submit" formmethod="post">POST</button></p>
 	</form>
 	'''
 	return Markup(html)
 
 
-@app.route('/test', methods=['GET', 'POST'])
-def test():
+@app.route('/getimgs', methods=['POST'])
+def getimgs():
 	try:
 		if request.method == 'POST':
+			path = request.form['path']
+			if os.path.exists(path) is False:
+				os.makedirs(path)
+			sys.path.append(os.path.dirname(__file__) + "/app")
 			import urlutil
 			obj = urlutil.Urlutil()
-			getImgs(request.form['url'], request.form['path'])
-			return request.form['url']
+			obj.getImgs(request.form['url'], path)
+			return request.form['url'] + '</br><a href="#" onclick="history.back(); return false;">back</a>'
 		else:
-			print(request.headers)
-			print(request.get_data())
-			return request.args.get('url', '')
+			return request.args.get('url', '') + '</br><a href="#" onclick="history.back(); return false;">back</a>'
 	except Exception as e:
-		return str(e)
+		return str(e) + '</br><a href="#" onclick="history.back(); return false;">back</a>'
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=8080, threaded=True)
